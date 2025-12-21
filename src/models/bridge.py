daf4c93c3568b58c1fc6d6a3a5f7e860dcc882b2
besidesmax <johannes.oelllerer@iu-study.org>
2025-12-21 19:44:31 +0100

    add 1)bridge_id is set automatically 2)that using a GridPoint for a Bridge, the "used" Attribut of the GridPoint is set to True

--- src/models/bridge.py
+++ src/models/bridge.py
@@ -1,21 +1,32 @@
 from models.bridge_type import BridgeType
 from models.node import Node
+from models.grid_point import GridPoint
 
 
 class Bridge:
     """Represents a network connection (Bridge) in the game."""
 
-    def __init__(self, bridge_id: int, from_node: Node, grid_points: list, to_node: Node, bridge_type: BridgeType):
+    id_counter = 1
+
+    def __init__(self, from_node: Node, grid_points: list[GridPoint], to_node: Node,
+                 bridge_type: BridgeType):
         """Initialize a bridge between two nodes.
 
         Args:
-            bridge_id: Unique identifier of the bridge.
             from_node: Start node of the connection.
             grid_points: List of GridPoints located along the bridge's path.
             to_node: End node of the connection.
             bridge_type: Type of this bridge, defines bandwidth and cost.
         """
-        self.bridge_id = bridge_id
+        # sets the bridge_id
+        self.bridge_id = Node.id_counter
+        Node.id_counter += 1
+
+        # test if GridPoint is already used
+        for grid_point in grid_points:
+            if grid_point.used:
+                raise ValueError("GridPoint", grid_point.grid_point_id, "is already used")
+            grid_point.used = True
         self.from_node = from_node
         self.grid_points = grid_points
         self.to_node = to_node
