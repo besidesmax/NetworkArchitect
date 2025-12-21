377d1e9c6d09db835116aa04409d87ff12e13478
besidesmax <johannes.oelllerer@iu-study.org>
2025-12-21 19:07:04 +0100

    change attribute grid_point_id from define manuel to define automatically

--- src/models/grid_point.py
+++ src/models/grid_point.py
@@ -1,17 +1,17 @@
 class GridPoint:
     """Represents a GridPoint on the board."""
 
-    def __init__(self, grid_point_id: int, x: int, y: int):
+    id_counter = 1
+
+    def __init__(self, x: int, y: int):
         """Initialize a GridPoint with position
 
         Args:
-            grid_point_id: Unique identifier of the GridPoint on the board.
             x: X coordinate of the GridPoint on the board.
             y: Y coordinate of the GridPoint on the board.
-            used: shows False if GridPoint isn't used and shows True if it is used
         """
-        self.grid_point_id = grid_point_id
+        self.grid_point_id = GridPoint.id_counter
+        GridPoint.id_counter += 1
         self.position_x = x
         self.position_y = y
         self.used = False
-
