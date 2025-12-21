eaa1d3000dc9d3924979a2a5bd4f15a509d1dc9b
besidesmax <johannes.oelllerer@iu-study.org>
2025-12-21 18:36:09 +0100

    add Attribut "used" to class GridPoint

--- src/models/grid_point.py
+++ src/models/grid_point.py
@@ -8,8 +8,10 @@ class GridPoint:
             grid_point_id: Unique identifier of the GridPoint on the board.
             x: X coordinate of the GridPoint on the board.
             y: Y coordinate of the GridPoint on the board.
+            used: shows False if GridPoint isn't used and shows True if it is used
         """
         self.grid_point_id = grid_point_id
         self.position_x = x
         self.position_y = y
+        self.used = False
 
