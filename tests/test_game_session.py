0b640b78d3c21ac1268e25afc35d8b3e40e8881e
besidesmax <johannes.oelllerer@iu-study.org>
2025-12-28 01:51:20 +0100

    test(tes_games_session): test for calculate_redundancy_score added !not finished!

--- tests/test_game_session.py
+++ tests/test_game_session.py
@@ -113,3 +113,41 @@ def test_is_it_solved_complete():
     session.place_bridge(node3, [session.level.game_board[22]], node4, BridgeType.ETHERNET)
 
     assert session.is_it_solved()
+
+    session.remove_bridge(session.network.bridges[0])
+
+    assert session.is_it_solved() is False
+
+
+def test_calculate_redundancy_score():  # TODO not finished
+    player1 = Player("TestPlayer")
+    level1 = Level(Difficulty.LIGHT, 1000, 5000)
+    session = GameSession(player1, level1)
+
+    # add Nodes
+    node1 = Node([session.level.game_board[2]], NodeType.CLIENT)
+    node2 = Node([session.level.game_board[41]], NodeType.CLIENT)
+    node3 = Node([session.level.game_board[21]], NodeType.SERVER)
+    node4 = Node([session.level.game_board[23]], NodeType.CLIENT)
+    node5 = Node([session.level.game_board[25]], NodeType.CLIENT)
+    node6 = Node([session.level.game_board[0]], NodeType.CLIENT)
+
+    # add node_config
+    session.level.node_config.nodes = [node1, node2, node3, node4, node5, node6]
+
+    # add bridges
+    session.place_bridge(node1, [session.level.game_board[3], session.level.game_board[12]], node3,
+                         BridgeType.FIBER)
+    session.place_bridge(node3, [session.level.game_board[30],
+                                 session.level.game_board[31],
+                                 session.level.game_board[32]], node2, BridgeType.WIFI_24G)
+    session.place_bridge(node3, [session.level.game_board[20],
+                                 session.level.game_board[29],
+                                 session.level.game_board[38],
+                                 session.level.game_board[39],
+                                 session.level.game_board[40]], node2, BridgeType.ETHERNET)
+    session.place_bridge(node4, [session.level.game_board[24]], node5, BridgeType.ETHERNET)
+    session.place_bridge(node6, [session.level.game_board[1]], node1, BridgeType.ETHERNET)
+    session.place_bridge(node3, [session.level.game_board[22]], node4, BridgeType.ETHERNET)
+
+    assert session.calculate_redundancy_score() == 2
