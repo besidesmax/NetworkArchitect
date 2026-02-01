4f3ac9e454bf7436e096dfe2f8a76966f3d61e9f
besidesmax <johannes.oelllerer@iu-study.org>
2026-02-01 19:10:08 +0100

    refactor

--- src/views/splash_screen.py
+++ src/views/splash_screen.py
@@ -1,7 +1,7 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QApplication
 from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 
 
 class SplashScreen(QWidget):
